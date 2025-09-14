7329b12ebf163fc9982be90dc7b80bf0292f7fce
payal723 <payaljaat55@gmail.com>
2025-09-14 09:04:24 +0530

    Add render build & start scripts

--- backend/server.js
+++ backend/server.js
@@ -22,17 +22,17 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 
+/* ---------- Serve React build ---------- */
+const buildPath = path.join(__dirname, 'frontend', 'build');
+app.use(express.static(buildPath));
 
-
-
-
+app.get('*', (req, res) =>
+  res.sendFile(path.join(buildPath, 'index.html'))
+);
 
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
